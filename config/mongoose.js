ac6b5d6c59531d83cda31db776090901c7c65a52
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2022-05-18 15:16:56 +0530

    change in mongoose file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
-const env = require('./environment');
 const mongoose = require('mongoose');
 async function main(){
-    await mongoose.connect(env.mongoose_path);
+    await mongoose.connect('mongodb+srv://placement:1UbhMXi13RXuCwSn@cluster0.p5gbb.mongodb.net/placementcell?retryWrites=true&w=majority');
     console.log("connection Successfull !! ");
 }
 main().catch(error =>console.log("connection not successfull !!"));
