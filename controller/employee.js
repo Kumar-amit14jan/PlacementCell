db28e3632271c06654e61d45d6d1a6b1d1d494f5
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2022-06-12 12:51:59 +0530

    add validation in server side in signup page

--- controller/employee.js
+++ controller/employee.js
@@ -8,7 +8,7 @@ module.exports.SignInPage = async function (req, res) {
 }
 module.exports.SignIn = async function (req, res) {
     try {
-        req.flash('success' , 'Sign In SuccessFully');
+        req.flash('success', 'Sign In SuccessFully');
         return res.redirect('/employee/dashboard');
     } catch (error) {
         return res.send('<h1>Error in SignIn</h1>');
@@ -16,46 +16,80 @@ module.exports.SignIn = async function (req, res) {
 }
 // sign up page for employee
 module.exports.createSessionPage = async function (req, res) {
-    
+
     return res.render('signUp', {
-        title: "Sign Up"
+        title: "Sign Up",
+        firstNameError: "",
+        lastNameError: "",
+        emailError: "",
+        passwordError: ""
     });
 }
 module.exports.createSession = async function (req, res) {
     try {
-        if(req.body.firstname.length === 0){
-            req.flash('error' , 'FirstName is not empty');
-            return res.redirect('back');
+        if (req.body.firstname.length === 0) {
+            return res.render('signUp', {
+                title: "Sign Up",
+                firstNameError: 'FirstName cannot blank',
+                lastNameError: "",
+                emailError: "",
+                passwordError: ""
+            });
         }
-        if(!isNaN(req.body.firstname)){
-            req.flash('error' , 'FirstName is not number');
-            return res.redirect('back');
+        if (!isNaN(req.body.firstname)) {
+            return res.render('signUp', {
+                title: "Sign Up",
+                firstNameError: 'FirstName is not number',
+                lastNameError: "",
+                emailError: "",
+                passwordError: ""
+            });
         }
         // for lastname
-        if(req.body.lastname.length === 0){
-            req.flash('error' , 'LastName is not empty');
-            return res.redirect('back');
+        if (req.body.lastname.length === 0) {
+            return res.render('signUp', {
+                title: "Sign Up",
+                firstNameError: "",
+                lastNameError: 'LastName is not empty',
+                emailError: "",
+                passwordError: ""
+            });
         }
-        if(!isNaN(req.body.lastname)){
-            req.flash('error' , 'LastName is not number');
-            return res.redirect('back');
+        if (!isNaN(req.body.lastname)) {
+            return res.render('signUp', {
+                title: "Sign Up",
+                firstNameError: "",
+                lastNameError: 'LastName is not number',
+                emailError: "",
+                passwordError: ""
+            });
         }
         // check on email
         if (!validator.isEmail(req.body.email)) {
-            req.flash('error' , 'Please Enter Valid Email ');
-            return res.redirect('back');
+            req.flash('error', '');
+            return res.render('signUp', {
+                title: "Sign Up",
+                firstNameError: "",
+                lastNameError: "",
+                emailError: 'Please Enter Valid Email'
+            });
         } else if (req.body.password.length < 2) {
-            req.flash('error' , 'Password is Small !!');
-            return res.redirect('back');
+            return res.render('signUp', {
+                title: "Sign Up",
+                firstNameError: "",
+                lastNameError: "",
+                emailError: "",
+                passwordError: 'Password is Small !!'
+            });
         } else {
             const employeePresent = await Employee.findOne({ email: req.body.email });
             if (employeePresent) {
-                req.flash('error' , 'Employee Already Exist !!');
+                req.flash('error', 'Employee Already Exist !!');
                 return res.redirect('back');
             } else {
                 const registerEmployee = await Employee(req.body);
                 registerEmployee.save();
-                req.flash('success' , 'Sign Up SuccessFully !!');
+                req.flash('success', 'Sign Up SuccessFully !!');
                 return res.redirect('/');
             }
         }
@@ -67,8 +101,8 @@ module.exports.createSession = async function (req, res) {
 }
 
 // to sign out
-module.exports.SignOut = async function(req ,res){
+module.exports.SignOut = async function (req, res) {
     req.logout();
-    req.flash('success' , 'Sign Out SuccessFully');
+    req.flash('success', 'Sign Out SuccessFully');
     return res.redirect('/');
 }
